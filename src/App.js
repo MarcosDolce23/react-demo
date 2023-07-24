078f568cebf8e2595313ed354020f12d1b137f50
Marcos Dolce <marcosdolce.91@gmail.com>
2023-07-24 20:54:36 -0300

    Card component from MUI added to the project
    items state rename to characters

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [item, setItems] = useState(null);
+  const [characters, setCharacters] = useState(null);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
@@ -14,13 +21,13 @@ function App() {
       .then(
         (result) => {
           setIsLoaded(true);
-          setItems(result);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
-    )
+          setCharacters(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
   }, [])
 
   if (error) {
@@ -29,13 +36,31 @@ function App() {
     return <div>Loading...</div>
   } else {
     return (
-      <ul>
-        {item.results.map(item => (
-          <li key={item.id}>
-            {item.name}
-          </li>
+      <>
+        {characters.results.map(item => (
+          <Card key={item.id} sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                {item.name}
+              </Typography>
+              {/* <Typography variant="h5" component="div">
+                be{bull}nev{bull}o{bull}lent
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography> */}
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
         ))}
-      </ul>
+      </>
     );
   }
 }
