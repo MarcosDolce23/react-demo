b858630d38a081a88365c9b8a04eb2eb58e12be7
Marcos Dolce <marcosdolce.91@gmail.com>
2023-07-24 20:13:48 -0300

    Api call completed
    I used the useEffect hook to call the api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+  const [item, setItems] = useState(null);
 
-  return (
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  }, [])
 
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <ul>
+        {item.results.map(item => (
+          <li key={item.id}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default App;
