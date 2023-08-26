2482c36713713e7cbca5012b2f5c23bf0aec4f45
Marcos Dolce <marcosdolce.91@gmail.com>
2023-08-26 16:19:33 -0300

    Descripción actualizada

--- src/about.js
+++ src/about.js
@@ -9,12 +9,12 @@ export default function About() {
       <div id="about" style={{ width: '100%', marginTop: "100px" }} classNameName="w3-main w3-content w3-padding">
 
         <div className="w3-container w3-padding-32 w3-center">
-          <h3>About Me, The Food Man</h3>
+          <h3>Acerca de mí, El Programador</h3>
           <img src="https://www.w3schools.com/w3images/chef.jpg" alt="Me" className="w3-image" style={{ display: "block", margin: "auto", width: "800px", height: "533px" }}></img>
           <div className="w3-padding-32">
-            <h4><b>I am Who I Am!</b></h4>
-            <h6><i>With Passion For Real, Good Food</i></h6>
-            <p>Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an interest of lorem ipsum and mauris neque quam blog. I want to share my world with you. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+            <h4><b>Yo soy Marcos y Marcos soy yo</b></h4>
+            <h6><i>Con pasión por la verdadera progamación</i></h6>
+            <p>Desde que tuve mi primer encuentro con líneas de código, supe que había descubierto mi verdadera pasión. La programación no es simplemente un conjunto de instrucciones para mí, es el lenguaje a través del cual puedo dar vida a mis ideas y crear soluciones que impactan en el mundo. Cada proyecto se convierte en un emocionante rompecabezas que ansío resolver, utilizando lógica, creatividad y una dosis de paciencia. Mi viaje en el mundo de la programación no solo me ha permitido adquirir habilidades técnicas, sino que también me ha enseñado a abrazar el aprendizaje constante y a enfrentar desafíos con determinación. A medida que navego a través de esta apasionante travesía, estoy emocionado por descubrir las infinitas posibilidades que la programación ofrece y por contribuir con mi creatividad al panorama tecnológico en constante evolución.</p>
           </div>
         </div>
       </div>
