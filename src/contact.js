8631fe5b90ccd3b34bd13070d8d76c4927fdec02
Marcos Dolce <marcosdolce.91@gmail.com>
2023-08-27 13:06:10 -0300

    fix favorite

--- src/contact.js
+++ src/contact.js
@@ -1,76 +1,82 @@
-import { Form } from "react-router-dom";
+import { useState } from 'react';
 import Sidebar from "./components/sidebar";
 import TopMenu from "./components/topMenu";
 
 export default function Contact() {
+
+  const [favorite, setFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setFavorite(!favorite);
+  };
+
   const contact = {
     first: "Marcos",
     last: "Dolce",
     avatar: "https://placekitten.com/g/200/200",
     twitter: "@MarcosDolce",
-    notes: "Estoy haciendo una demo con ReactJs",
-    favorite: true,
+    facebook: "marcos.dolce",
+    github: "MarcosDolce23",
+    glo: "marcos.dolce",
+    notes: "Estoy haciendo una demo con ReactJs"
   };
 
   return (
     <>
       <Sidebar></Sidebar>
       <TopMenu></TopMenu>
-    <div id="contact" style={{ width: '100%', marginTop: "100px" }} className="w3-main w3-content w3-padding">
-      <div>
-        <img
-          key={contact.avatar}
-          src={contact.avatar || null}
-        />
+      <div id="contact" style={{ width: '100%', marginTop: "100px" }} className="w3-main w3-content w3-padding">
+        <div>
+          <img
+            key={contact.avatar}
+            src={contact.avatar || null}
+          />
+        </div>
+
+        <div>
+          <h1>
+            {contact.first || contact.last ? (
+              <>
+                {contact.first} {contact.last}
+              </>
+            ) : (
+              <i>No Name</i>
+            )}{" "}
+            <Favorite favorite={favorite} handleClick={toggleFavorite}/>
+          </h1>
+
+          {contact.twitter && (
+            <p>
+              <a
+                target="_blank"
+                href={`https://twitter.com/${contact.twitter}`}
+              >
+                {contact.twitter}
+              </a>
+            </p>
+          )}
+
+          {contact.notes && <p>{contact.notes}</p>}
+
+        </div>
       </div>
-
-      <div>
-        <h1>
-          {contact.first || contact.last ? (
-            <>
-              {contact.first} {contact.last}
-            </>
-          ) : (
-            <i>No Name</i>
-          )}{" "}
-          <Favorite contact={contact} />
-        </h1>
-
-        {contact.twitter && (
-          <p>
-            <a
-              target="_blank"
-              href={`https://twitter.com/${contact.twitter}`}
-            >
-              {contact.twitter}
-            </a>
-          </p>
-        )}
-
-        {contact.notes && <p>{contact.notes}</p>}
-
-      </div>
-    </div>
     </>
   );
 }
 
-function Favorite({ contact }) {
-  // yes, this is a `let` for later
-  let favorite = contact.favorite;
+function Favorite({ favorite, handleClick }) {
   return (
-    <Form method="post">
-      <button
-        name="favorite"
-        value={favorite ? "false" : "true"}
-        aria-label={
-          favorite
-            ? "Remove from favorites"
-            : "Add to favorites"
-        }
-      >
-        {favorite ? "★" : "☆"}
-      </button>
-    </Form>
+    <button
+      name="favorite"
+      value={favorite ? "false" : "true"}
+      onClick={handleClick}
+      aria-label={
+        favorite
+          ? "Remove from favorites"
+          : "Add to favorites"
+      }
+    >
+      {favorite ? "★" : "☆"}
+    </button>
   );
 }
