a4d06273e88e00b9a16bc2f90232ca9d824518af
Marcos Dolce <marcosdolce.91@gmail.com>
2023-08-27 13:43:58 -0300

    Fix console error

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -10,7 +10,7 @@ export default function Sidebar() {
 
     return (
         <nav className="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style={{ display: "none", zIndex: "2", width: "40%", minWidth: "300px" }} id="mySidebar">
-            <a href="javascript:void(0)" onClick={w3_close}
+            <a onClick={w3_close}
                 className="w3-bar-item w3-button">Close Menu</a>
             <Link onClick={w3_close} className="w3-bar-item w3-button" to={'/contacts'}>Contacts</Link>
             <Link onClick={w3_close} className="w3-bar-item w3-button" to={'/'}>Home</Link>
