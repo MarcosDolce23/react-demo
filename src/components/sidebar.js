44e34a887068bd82c31efcd54c5587bf761db014
Marcos Dolce <marcosdolce.91@gmail.com>
2023-08-28 21:15:30 -0300

    Agrego MUI al sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import "./components.css";
+
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
 import { Link } from 'react-router-dom';
 
-export default function Sidebar({ handleToggle, showed}) {
-    
-    if (!showed) {
-        return <></>;
-    } else {
-        return (
-            <nav className="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style={{ display: "block", zIndex: "2", width: "40%", minWidth: "300px" }} id="mySidebar">
-                <a onClick={handleToggle}
-                    className="w3-bar-item w3-button">Close Menu</a>
-                <Link onClick={handleToggle} className="w3-bar-item w3-button" to={'/contacts'}>Contacts</Link>
-                <Link onClick={handleToggle} className="w3-bar-item w3-button" to={'/'}>Home</Link>
-                <Link onClick={handleToggle} className="w3-bar-item w3-button" to={'/about'}>About</Link>
-            </nav>
-        )
-    }
+export default function Sidebar({ handleToggle, showed }) {
+
+    return (
+        <SwipeableDrawer open={showed} onClose={handleToggle} onOpen={handleToggle} >
+            <Box
+                sx={{ width: 250 }}
+                role="presentation"
+                // onClick={handleToggle}
+            >
+                <List>
+                    <ListItem key={"Home"} disablePadding>
+                        <Link onClick={handleToggle} to={'/'}>
+                            <ListItemButton>
+                                <ListItemText primary={"Home"} />
+                            </ListItemButton>
+                        </Link>
+                    </ListItem>
+                    <ListItem key={"About"} disablePadding>
+                        <Link onClick={handleToggle} to={'/about'}>
+                            <ListItemButton>
+                                <ListItemText primary={"Sobre mi"} />
+                            </ListItemButton>
+                        </Link>
+                    </ListItem>
+                    <ListItem key={"Contact"} disablePadding>
+                        <Link onClick={handleToggle} to={'/contacts'}>
+                            <ListItemButton>
+                                <ListItemText primary={"Contacto"} />
+                            </ListItemButton>
+                        </Link>
+                    </ListItem>
+                </List>
+            </Box>
+        </SwipeableDrawer>
+    )
 }
--- src/components/topMenu.js
+++ src/components/topMenu.js
@@ -10,36 +10,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 import Sidebar from './sidebar';
 
-// import Sidebar from "./sidebar";
-// import "./components.css";
-
-// export default function TopMenu() {
-
-//     const [showSidebar, setShowSidebar] = useState(false);
-
-//     const toggleSidebar = () => {
-//         setShowSidebar(!showSidebar);
-//     };
-
-//     function w3_open() {
-//         document.getElementById("mySidebar").style.display = "block";
-//     }
-
-//     return (
-//         <>
-//             <Sidebar handleToggle={toggleSidebar} showed={showSidebar}></Sidebar>
-//             <div className="w3-top">
-//                 <div className="w3-white w3-xlarge" style={{ maxWidth: "1200px", margin: "auto" }}>
-//                     <div className="w3-button w3-padding-16 w3-left" onClick={toggleSidebar}>☰</div>
-//                     <div className="w3-right w3-padding-16">Marcos Dolce</div>
-//                     <div className="w3-center w3-padding-16">React Demo GL</div>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
-
-
 export default function TopMenu() {
 
     const [showSidebar, setShowSidebar] = useState(false);
@@ -50,7 +20,7 @@ export default function TopMenu() {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-        <Sidebar handleToggle={toggleSidebar} showed={showSidebar}></Sidebar>
+            <Sidebar handleToggle={toggleSidebar} showed={showSidebar}></Sidebar>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
